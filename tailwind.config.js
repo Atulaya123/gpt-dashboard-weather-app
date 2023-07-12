0fbd4ddd6f6ca03e5988e2eceab886b687ca7ae8
Atulaya123 <arai2_be20@thapar.edu>
2023-07-13 01:13:42 +0530

    Adding Tremor Library
    Adding Tremor Library Which helps in building interactive dashboards

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}', // Tremor module
+  
   ],
   theme: {
     extend: {
